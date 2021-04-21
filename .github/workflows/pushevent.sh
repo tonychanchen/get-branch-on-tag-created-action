471af7dbe15f4fdce6d34f55b3a9657e29800b96
tonychanchen <tonychanchen@gmail.com>
2021-04-21 18:47:24 +0800

    Update pushevent.sh

--- .github/workflows/pushevent.sh
+++ .github/workflows/pushevent.sh
@@ -1,7 +1,7 @@
 rtt=$(git describe --tags `git rev-list --tags --max-count=1`)
 rc=$(git rev-parse --short HEAD)
 rb=$(git rev-parse --abbrev-ref HEAD)
-echo $rtt
-echo $rc
-echo $rb
-echo $GIT_BRANCH_TAG
+echo 111-$rtt
+echo 222-$rc
+echo 333-$rb
+echo 444-$GIT_BRANCH_TAG
