f58a6cb872c6d04cabbd1b8f93e6f04af7947a47
eagleychen(陈鹰) <tonychanchen@gmail.com>
2021-04-22 17:17:09 +0800

    ttt

--- .github/workflows/pushevent.sh
+++ .github/workflows/pushevent.sh
@@ -1,7 +1,9 @@
 rtt=$(git describe --tags `git rev-list --tags --max-count=1`)
+rttt=$(git describe --tags --abbrev=0)
 rc=$(git rev-parse --short HEAD)
 rb=$(git rev-parse --abbrev-ref HEAD)
 echo 111-$rtt
+echo 111-$rttt
 echo 222-$rc
 echo 333-$rb
 echo 444-$GIT_BRANCH_TAG
